4b24b7ddd38f8dd4c45e28e217bde17863edebff
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2016-10-04 13:51:11 -0700

    Add grunt files, images and grunt optimized images

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.initConfig({
+  responsive_images: {
+    dev: {
+      options: {
+        engine: 'im',
+        sizes: [{
+          suffix: '_small',
+          width: 1600,
+          quality: 30
+
+        }]
+      },
+
+      files: [{
+        expand: true,
+        src: ['*.{gif,jpg,png}'],
+        cwd: 'images_src/',
+        dest: 'images/'
+      }]
+    }
+  },
+
+  cssmin: {
+    target: {
+      files: [{
+        expand: true,
+        cwd: 'css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'css/min/',
+        ext: '.min.css'
+    }]
+  }
+}
+});
+  grunt.register.Task('default',[
+    'responsive_images',
+    'cssmin'
+  ])
+};
